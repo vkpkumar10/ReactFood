40ec5513bbdb6020c61d08fb28dc341bd33c69f9
PULIMURUGAN\vkpku <vkpkumar@hotmail.com>
2023-09-27 12:07:58 -0400

    React Food app commit on 9/27/2023

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,48 @@
+import React from "react";
+import classes from "./header.module.css";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  const user = {
+    name: "PradeepKumar",
+  };
+
+  const cart = {
+    totalcount: 10,
+  };
+
+  const logout = () => {};
+
+  return (
+    <header className={classes.header}>
+      <div className={classes.container}>
+        <Link to="/" className={classes.logo}>
+          Food with React!
+        </Link>
+
+        <nav>
+          <ul>
+            {user ? (
+              <li className={classes.menu_container}>
+                <Link to="/profile">{user.name}</Link>
+                <div className={classes.menu}>
+                  <Link to="/Profile">Profile</Link>
+                  <Link to="/Orders">Orders </Link>
+                </div>
+              </li>
+            ) : (
+              <Link to="/Login">Login</Link>
+            )}
+
+            <li>
+              <Link to="/Cart">
+                Cart
+                {cart.totalcount > 0 && <span className={classes.cart_count} >{cart.totalcount} </span>}
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
